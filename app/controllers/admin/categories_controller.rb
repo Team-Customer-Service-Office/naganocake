3a9a53db3958e10feda50046616ad5cdf3cc4d7b
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-18 08:12:26 +0000

    [Make]Categories View

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,13 +1,29 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @category = Category.new
+    @categories = Category.all
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      @categories = Category.all
+      render :index, notice: '新規登録が完了しました。'
+    else
+      @categories = Category.all
+      render :index
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
