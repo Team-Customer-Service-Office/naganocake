cfb0299573b2ce8bc471acea445ae6f97944ca0a
nonoka57 <nonoka57nonoka@gmail.com>
2021-11-20 08:02:21 +0000

    [Update]address,order

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,16 +1,49 @@
 class AddressesController < ApplicationController
+  
   def index
+    @address = Address.new
+    # @addresses = Address.find(params[:id])
+    
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_user.id
+    if @address.save
+      flash[:notice]="You have created book successfully."
+      redirect_to address_path
+    else
+      # @addresses = Address.find(params[:id])
+      render :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to address_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to address_path
   end
+
+
+
+private
+
+  def address_params
+      params.require(:address).permit(:postcode, :address, :full_name)
+  end
+  
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,19 @@
 class Order < ApplicationRecord
     
     
+    belongs_to :costomer
+
+    
+    validates :postcode, presence: true, length: {maximum: 7}
+    validates :address, presence: true
+    validates :full_name, presence: true
+    validates :payment_method, presence: true
+    
+    enum payment_method: { credit_card: 0, transfer: 1 }
+      
+    enum order_status: { wait:0, confirm:1, production:2, preparation:3, shipped:4 }
+      
+  
+    
+    
 end
