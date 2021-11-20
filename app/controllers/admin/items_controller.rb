922569085dc4a46db42552284915307291e2f585
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-20 05:07:54 +0000

    [Update]Items Views

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,9 +9,15 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      redirect_to new_admin_item_path
+    end
   end
 
   def show
+    @item - Item.find(params[:id])
   end
 
   def edit
@@ -19,4 +25,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:category_id, :name, :description, :price_without_tax, :image, :is_active)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
   
   belongs_to :category
-  enum is_active: { stop: 0, sale: 1 }
+  enum is_active: { stop: false, sale: true }
+  
+  attachment :image
+  
+  validates :name, {presence: true}
+  validates :description, {presence: true}
+  validates :price_without_tax, {presence: true}
+  
 end
