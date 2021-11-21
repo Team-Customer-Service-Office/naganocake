3215220b4fcf8d778391994d7d992502603a75f5
enddrop28 <mea.enddrop@gmail.com>
2021-11-21 10:27:26 +0000

    [Make]admin-customers-show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CustomersController < ApplicationController
-  before_action :authenticate_admin!
+  # before_action :authenticate_admin!
 
 
   def index
@@ -8,6 +8,7 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
