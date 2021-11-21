90925e759f7229c5ea0dea8c782f9b4c86384d00
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-21 07:17:19 +0000

    [Fix]Admin Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   
   belongs_to :category
-  enum is_active: { stop: false, sale: true }
+  enum is_active: { stop: 0, sale: 1 }
   
   attachment :image
   
-  validates :name, {presence: true}
-  validates :description, {presence: true}
-  validates :price_without_tax, {presence: true}
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, presence: true
+  validates :price_without_tax, presence: true
   
 end
