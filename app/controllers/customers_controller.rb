b2b62e63f1dc0b72ce8e7118b50501183bfeda02
enddrop28 <mea.enddrop@gmail.com>
2021-11-22 10:44:19 +0000

    [Make]customers-customer-show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,9 @@
 class Customer::CustomersController < ApplicationController
+  # before_action :authenticate_customer!
+  # before_action :ensure_correct_customer, {only: [:show, :edit]}
+
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
@@ -13,4 +17,18 @@ class Customer::CustomersController < ApplicationController
 
   def out
   end
+
+  privete
+
+  def customer_params
+    params.require(:customer).permit(:is_deleted, :last_name, :first_name, :last_name_kana, :first_name_kana,:phone_number, :email, :password, :postcode, :address)
+  end
+
+  def ensure_correct_customer
+    @customer = Customer.find(params[:id])
+    if current_customer.id != @customer.id
+      redirect_to root_path
+    end
+  end
+
 end
